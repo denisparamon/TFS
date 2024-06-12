efd5bc72af957af63b7bebc4c83a82f3a8e5ca3e
Hooman <denis_paramon@icloud.com>
2024-06-12 15:37:15 +0500

    initialCommit

---
+++ public/index.php
@@ -0,0 +1,39 @@
+<?php
+
+use Slim\Factory\AppFactory;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Nyholm\Psr7\Factory\Psr17Factory;
+use Nyholm\Psr7Server\ServerRequestCreator;
+use Nyholm\Psr7Server\ServerRequestCreatorInterface;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+// Create AppFactory with PSR-17 implementations
+$psr17Factory = new Psr17Factory();
+$responseFactory = $psr17Factory;
+$streamFactory = $psr17Factory;
+$serverRequestCreator = new ServerRequestCreator(
+    $psr17Factory,
+    $psr17Factory,
+    $psr17Factory,
+    $psr17Factory
+);
+
+// Create App with response factory
+AppFactory::setResponseFactory($responseFactory);
+AppFactory::setStreamFactory($streamFactory);
+$app = AppFactory::create();
+
+// Define the route
+$app->get('/hello/{name}', function (Request $request, Response $response, $args) {
+    $name = $args['name'];
+    $response->getBody()->write("Hello, $name");
+    return $response;
+});
+
+// Create ServerRequest from globals
+$serverRequest = $serverRequestCreator->fromGlobals();
+
+// Run app with ServerRequest
+$app->run($serverRequest);
